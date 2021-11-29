37d3add7f961e02c7da6154f23a02a0157d2f39e
Josh Green <emailcoder@gmail.com>
2021-11-29 15:47:53 +0000

    Revert "adding mu plugins"
    This reverts commit 6d590214c0a40c0a7e4abb820c5e15ad5eac5858.

--- wp-content/mu-plugins/thanks-for-reading.php
+++
@@ -1,4 +0,0 @@
-<?php
-add_filter( 'the_content', function( $content ) {
-    return $content . '<p>Got any questions about this article? Just pop me <a href="mailto:josh@joshgreendesign.com">an email</a>.</p>';
-} );
